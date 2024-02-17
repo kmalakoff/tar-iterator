2b27f2ce903e3dba978aaad7a73868be52c18a65
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-17 12:58:23 -0800

    Upgrade to ts-dev-stack

--- test/spec/asyncAwait.js
+++ test/spec/asyncAwait.js
@@ -1,17 +1,17 @@
 require('../lib/patch');
-var assert = require('assert');
-var rimraf = require('rimraf');
-var mkpath = require('mkpath');
-var path = require('path');
-var assign = require('just-extend');
+const assert = require('assert');
+const rimraf = require('rimraf');
+const mkpath = require('mkpath');
+const path = require('path');
+const assign = require('just-extend');
 
-var TarIterator = require('../..');
-var validateFiles = require('../lib/validateFiles');
+const TarIterator = require('tar-iterator');
+const validateFiles = require('../lib/validateFiles');
 
-var constants = require('../lib/constants');
-var TMP_DIR = constants.TMP_DIR;
-var TARGET = constants.TARGET;
-var DATA_DIR = constants.DATA_DIR;
+const constants = require('../lib/constants');
+const TMP_DIR = constants.TMP_DIR;
+const TARGET = constants.TARGET;
+const DATA_DIR = constants.DATA_DIR;
 
 async function extract(iterator, dest, options) {
   const links = [];
@@ -32,7 +32,7 @@ async function extract(iterator, dest, options) {
 async function extractForEach(iterator, dest, options) {
   const links = [];
   await iterator.forEach(
-    async function (entry) {
+    async (entry) => {
       if (entry.type === 'link') links.unshift(entry);
       else if (entry.type === 'symlink') links.push(entry);
       else await entry.create(dest, options);
@@ -44,17 +44,17 @@ async function extractForEach(iterator, dest, options) {
   for (const entry of links) await entry.create(dest, options);
 }
 
-describe('asyncAwait', function () {
-  beforeEach(function (callback) {
-    rimraf(TMP_DIR, function (err) {
+describe('asyncAwait', () => {
+  beforeEach((callback) => {
+    rimraf(TMP_DIR, (err) => {
       if (err && err.code !== 'EEXIST') return callback(err);
       mkpath(TMP_DIR, callback);
     });
   });
 
-  describe('happy path', function () {
-    it('extract - no strip - concurrency 1', async function () {
-      var options = { now: new Date(), concurrency: 1 };
+  describe('happy path', () => {
+    it('extract - no strip - concurrency 1', async () => {
+      const options = { now: new Date(), concurrency: 1 };
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
@@ -63,8 +63,8 @@ describe('asyncAwait', function () {
       }
     });
 
-    it('extract - no strip - concurrency Infinity', async function () {
-      var options = { now: new Date(), concurrency: Infinity };
+    it('extract - no strip - concurrency Infinity', async () => {
+      const options = { now: new Date(), concurrency: Infinity };
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
@@ -73,8 +73,8 @@ describe('asyncAwait', function () {
       }
     });
 
-    it('extract - no strip - forEach', async function () {
-      var options = { now: new Date(), concurrency: Infinity };
+    it('extract - no strip - forEach', async () => {
+      const options = { now: new Date(), concurrency: Infinity };
       try {
         await extractForEach(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
@@ -83,8 +83,8 @@ describe('asyncAwait', function () {
       }
     });
 
-    it('extract - strip 1', async function () {
-      var options = { now: new Date(), strip: 1 };
+    it('extract - strip 1', async () => {
+      const options = { now: new Date(), strip: 1 };
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
@@ -93,8 +93,8 @@ describe('asyncAwait', function () {
       }
     });
 
-    it('extract multiple times', async function () {
-      var options = { now: new Date(), strip: 1 };
+    it('extract multiple times', async () => {
+      const options = { now: new Date(), strip: 1 };
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
@@ -112,9 +112,9 @@ describe('asyncAwait', function () {
     });
   });
 
-  describe('unhappy path', function () {
-    it('should fail with too large strip', async function () {
-      var options = { now: new Date(), strip: 2 };
+  describe('unhappy path', () => {
+    it('should fail with too large strip', async () => {
+      const options = { now: new Date(), strip: 2 };
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         assert.ok(false);
