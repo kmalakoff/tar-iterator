8bb606ac04c802cd35d4d20514e4bbd0e8fb0f4d
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-14 17:55:24 -0800

    Update dependencies and fix test error handling

--- test/unit/promise.test.cjs
+++ test/unit/promise.test.cjs
@@ -76,7 +76,7 @@ describe('promise', () => {
           entry.destroy();
         },
         (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           assert.ok(!iterator.extract);
           done();
         }
@@ -86,10 +86,10 @@ describe('promise', () => {
     it('extract - no strip - concurrency 1', (done) => {
       const options = { now: new Date(), concurrency: 1 };
       extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -98,10 +98,10 @@ describe('promise', () => {
     it('extract - no strip - concurrency Infinity', (done) => {
       const options = { now: new Date(), concurrency: Infinity };
       extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -111,10 +111,10 @@ describe('promise', () => {
       const options = { now: new Date() };
       const source = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar'));
       extract(new TarIterator(source), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -125,10 +125,10 @@ describe('promise', () => {
       let source = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar.bz2'));
       source = source.pipe(bz2());
       extract(new TarIterator(source), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -139,10 +139,10 @@ describe('promise', () => {
       let source = fs.createReadStream(path.join(DATA_DIR, 'fixture.tar.gz'));
       source = source.pipe(zlib.createUnzip());
       extract(new TarIterator(source), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -151,10 +151,10 @@ describe('promise', () => {
     it('extract - strip 1', (done) => {
       const options = { now: new Date(), strip: 1 };
       extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           done();
         });
       });
@@ -163,19 +163,19 @@ describe('promise', () => {
     it('extract multiple times', (done) => {
       const options = { now: new Date(), strip: 1 };
       extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options, (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
 
         validateFiles(options, 'tar', (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
 
           extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options, (err) => {
             assert.ok(err);
 
             extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, { force: true, ...options }, (err) => {
-              if (err) return done(err);
+              if (err) return done(err.message);
 
               validateFiles(options, 'tar', (err) => {
-                if (err) return done(err);
+                if (err) return done(err.message);
                 done();
               });
             });
