fcab58d93ee520ede4700df9d889fd0b09e6dd52
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-22 19:48:02 -0800

    Upgrade modules

--- src/TarIterator.mjs
+++ src/TarIterator.mjs
@@ -1,7 +1,6 @@
 import fs from 'fs';
-import eos from 'end-of-stream';
+import once from 'call-once-fn';
 import BaseIterator from 'extract-base-iterator';
-import Queue from 'queue-cb';
 import tarStream from 'tar-stream-compat';
 
 import Lock from './lib/Lock.mjs';
@@ -14,48 +13,26 @@ export default class TarIterator extends BaseIterator {
     this.lock = new Lock();
     this.lock.iterator = this;
 
-    const queue = new Queue(1);
     let cancelled = false;
-    function setup() {
+    const setup = () => {
       cancelled = true;
-    }
+    };
     this.processing.push(setup);
     this.extract = tarStream.extract();
 
-    queue.defer((callback) => {
+    const pipe = (cb) => {
       try {
         if (typeof source === 'string') source = fs.createReadStream(source);
       } catch (err) {
-        callback(err);
+        cb(err);
       }
 
-      let data = null;
-      function cleanup() {
-        source.removeListener('error', onError);
-        source.removeListener('data', onData);
-      }
-      function onError(err) {
-        data = err;
-        cleanup();
-        callback(err);
-      }
-      function onData() {
-        data = true;
-        cleanup();
-        callback();
-      }
-
-      source.on('error', onError);
-      source.on('data', onData);
-      eos(source.pipe(this.extract), (err) => {
-        if (data) return;
-        cleanup();
-        callback(err);
-      });
-    });
-
-    // start processing
-    queue.await((err) => {
+      const end = once(cb);
+      source.on('data', () => end());
+      source.on('error', end);
+      source.pipe(this.extract);
+    };
+    pipe((err) => {
       fifoRemove(this.processing, setup);
       if (this.done || cancelled) return; // done
       err ? this.end(err) : this.push(nextEntry.bind(null, null));
