cc14a0c3a936aa11f052061d6433e63e28857a02
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 14:39:55 -0700

    Comprehensive types

--- src/lib/Lock.ts
+++ src/lib/Lock.ts
@@ -1,14 +1,26 @@
 import BaseIterator from 'extract-base-iterator';
-import LC from 'lifecycle';
 
-export default LC.RefCountable.extend({
-  constructor: function () {
-    LC.RefCountable.prototype.constructor.call(this);
-  },
-  __destroy: function () {
+export default class Lock {
+  private count = 1;
+
+  // members
+  iterator: BaseIterator<unknown> = null;
+  err: Error = null;
+
+  retain() {
+    this.count++;
+  }
+
+  release() {
+    if (this.count <= 0) throw new Error('Lock count is corrupted');
+    this.count--;
+    if (this.count === 0) this.__destroy();
+  }
+
+  private __destroy() {
     if (this.iterator) {
       BaseIterator.prototype.end.call(this.iterator, this.err || null);
       this.iterator = null;
     }
-  },
-});
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
-export type { ExtractOptions } from 'extract-base-iterator';
+export type { default as Lock } from './lib/Lock.js';
+export { default as FileEntry } from './FileEntry.js';
+export { type Entry, DirectoryEntry, LinkEntry, SymbolicLinkEntry, type ExtractOptions } from 'extract-base-iterator';
 
-export interface LockT {
-  iterator?: unknown;
-  err?: Error;
-  retain: () => void;
-  release: () => void;
+import type { AbstractIterator, Entry } from 'extract-base-iterator';
+import type { default as Lock } from './lib/Lock.js';
+export interface AbstractTarIterator extends AbstractIterator<unknown> {
+  lock: Lock;
+  extract: NodeJS.WritableStream;
 }
+
+export type EntryCallback = (error?: Error, entry?: Entry) => undefined;
