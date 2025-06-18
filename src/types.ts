e2ed08a0c011b0bbff642071012aa7454b84ad35
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-18 07:43:28 -0700

    Upgrade modules

--- src/types.ts
+++ src/types.ts
@@ -2,11 +2,6 @@ export { DirectoryEntry, type Entry, type ExtractOptions, LinkEntry, SymbolicLin
 export { default as FileEntry } from './FileEntry.js';
 export type { default as Lock } from './lib/Lock.js';
 
-import type { AbstractIterator, Entry } from 'extract-base-iterator';
-import type { default as Lock } from './lib/Lock.js';
-export interface AbstractTarIterator extends AbstractIterator<unknown> {
-  lock: Lock;
-  extract: NodeJS.WritableStream;
-}
+import type { Entry } from 'extract-base-iterator';
 
 export type EntryCallback = (error?: Error, result?: IteratorResult<Entry>) => undefined;
