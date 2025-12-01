f59ecf9425a3c56e02e4a094c1135e8684c99880
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-01 11:58:32 -0800

    Fixed reentrant flows

--- src/lib/Lock.ts
+++ src/lib/Lock.ts
@@ -1,4 +1,5 @@
 import BaseIterator from 'extract-base-iterator';
+import type TarExtract from '../tar/TarExtract.ts';
 
 export default class Lock {
   private count = 1;
@@ -6,6 +7,11 @@ export default class Lock {
   // members
   iterator: BaseIterator = null;
   err: Error = null;
+
+  // cleanup resources
+  /** @internal @hidden */
+  extract: TarExtract = null;
+  sourceStream: NodeJS.ReadableStream = null;
 
   retain() {
     this.count++;
@@ -18,6 +24,19 @@ export default class Lock {
   }
 
   private __destroy() {
+    // Destroy source stream FIRST to stop data flow
+    if (this.sourceStream) {
+      const stream = this.sourceStream as NodeJS.ReadableStream & { destroy?: () => void };
+      if (typeof stream.destroy === 'function') stream.destroy();
+      this.sourceStream = null;
+    }
+
+    // Clear extract reference
+    if (this.extract) {
+      this.extract = null;
+    }
+
+    // Call BaseIterator.end() LAST
     if (this.iterator) {
       BaseIterator.prototype.end.call(this.iterator, this.err || null);
       this.iterator = null;
