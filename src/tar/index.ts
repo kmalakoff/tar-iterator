877d4dd3dfcdd0ca2a7144942488b52d98381b6f
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-01 18:43:24 -0800

    Shared BufferList

--- src/tar/index.ts
+++ src/tar/index.ts
@@ -4,7 +4,7 @@
  * Node 0.8+ compatible TAR extraction without external dependencies.
  */
 
-export { default as BufferList } from './BufferList.ts';
+export { BufferList } from 'extract-base-iterator';
 export * from './constants.ts';
 export * from './headers.ts';
 export { default as TarExtract, type TarExtractOptions } from './TarExtract.ts';
