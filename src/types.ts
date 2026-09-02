bf24c9f2ef9426578773fa19572af6f881901e0f
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-01 23:34:16 -0700

    Update module dependencies and relative import extensions

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export { DirectoryEntry, type Entry, type ExtractOptions, LinkEntry, SymbolicLinkEntry } from 'extract-base-iterator';
-export { default as FileEntry } from './FileEntry.js';
-export type { default as Lock } from './lib/Lock.js';
+export { default as FileEntry } from './FileEntry.ts';
+export type { default as Lock } from './lib/Lock.ts';
 
 import type { Entry } from 'extract-base-iterator';
 
