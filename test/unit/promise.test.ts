kmalakoff/walk-filtered

Replace rimraf2 with fs-remove-compat in tests

test/unit/promise.test.ts
@@ -1,9 +1,9 @@
 import assert from 'assert';
 import generate from 'fs-generate';
+import { safeRm } from 'fs-remove-compat';
 import statsSpys from 'fs-stats-spys';
 import path from 'path';
 import Pinkie from 'pinkie-promise';
-import rimraf2 from 'rimraf2';
 import url from 'url';
 
 import walk from 'walk-filtered';
@@ -37,13 +37,13 @@ describe('promise', () => {
 
   describe('setup tests', () => {
     beforeEach((done) => {
-      rimraf2(TEST_DIR, { disableGlob: true }, () => {
+      safeRm(TEST_DIR, () => {
         generate(TEST_DIR, STRUCTURE, (err) => {
           done(err);
         });
       });
     });
-    after((cb) => rimraf2(TEST_DIR, { disableGlob: true }, () => cb()));
+    after((cb) => safeRm(TEST_DIR, () => cb()));
 
     it('should be default false', async () => {
       const spys = statsSpys();

kmalakoff/tar-iterator

test/unit/promise.test.ts
@@ -1,10 +1,10 @@
 import assert from 'assert';
 import fs from 'fs';
+import { safeRm } from 'fs-remove-compat';
 import mkdirp from 'mkdirp-classic';
 import path from 'path';
 import Pinkie from 'pinkie-promise';
 import Queue from 'queue-cb';
-import rimraf2 from 'rimraf2';
 import TarIterator from 'tar-iterator';
 import zlib from 'zlib';
 import bz2 from '../lib/bz2-stream.ts';
@@ -52,7 +52,7 @@ describe('promise', () => {
   })();
 
   beforeEach((callback) => {
-    rimraf2(TARGET, { disableGlob: true }, () => {
+    safeRm(TARGET, () => {
       mkdirp(TARGET, callback);
     });
   });