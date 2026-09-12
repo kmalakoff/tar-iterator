c4485f64146a43d5cd459ca553865abda56cb352
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 20:55:51 -0700

    Add force flag and support for compressed streams

--- test/spec/asyncAwait.js
+++ test/spec/asyncAwait.js
@@ -1,7 +1,9 @@
+require('../lib/patch');
 var assert = require('assert');
 var rimraf = require('rimraf');
 var mkpath = require('mkpath');
 var path = require('path');
+var assign = require('object-assign');
 
 var TarIterator = require('../..');
 var validateFiles = require('../lib/validateFiles');
@@ -96,7 +98,13 @@ describe('asyncAwait', function () {
       try {
         await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
         await validateFiles(options, 'tar');
-        await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
+        try {
+          await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, options);
+          assert.ok(false);
+        } catch (err) {
+          assert.ok(err);
+        }
+        await extract(new TarIterator(path.join(DATA_DIR, 'fixture.tar')), TARGET, assign({ force: true }, options));
         await validateFiles(options, 'tar');
       } catch (err) {
         assert.ok(!err);
